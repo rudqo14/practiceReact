cf800255dca1510c4337e5f73ffc8d53e2c9bca5
김경배 <keyoungbaekim@gimgyeongbaeui-MacBookPro.local>
2021-04-22 22:36:30 +0900

    Add:
    navbar, Title 작성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
 
 function App() {
+  let [title] = useState(["KeyoungBae's Blog","나의 블로그"]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <div className="title-list">
+      <h3> { title[0] } </h3>
+      <p>4월 22일 발행</p>
+      <hr/>
+      </div>
+      <div>
+        <div className="boardList">
+          <div className="boardTitleContainer">
+            <p className="boardTitle">게시판 제목</p>
+            <p className="boardContent">내용</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
