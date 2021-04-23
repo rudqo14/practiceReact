5c34037be89131ea3b87601e035e45cbcc221286
김경배 <keyoungbaekim@gimgyeongbaeui-MacBookPro.local>
2021-04-23 23:24:08 +0900

    Add: LikeBtn을 눌러 숫자변경

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React, { useState } from 'react';
-import './App.scss';
+import React, { useState } from "react";
+import "./App.scss";
 
 function App() {
-  let [title] = useState(["KeyoungBae's Blog","나의 블로그"]);
+  let [title] = useState(["일상 생활", "개발 서적", "알고리즘"]);
+  let [like, setLike] = useState(0);
+  
+  const likeHandler = () => {
+    setLike(like+1);
+  }
 
   return (
     <div className="App">
-      <div className="black-nav">
+      <div className="blackNav">
         <div>개발 Blog</div>
       </div>
-      <div className="title-list">
-      <h3> { title[0] } </h3>
-      <p>4월 22일 발행</p>
-      <hr/>
+      <div className="boardList">
+        <h3>{title[0]}<span onClick={ ()=> { likeHandler(); } } >👍</span></h3>
+        <p>4월 22일 발행</p>
+        <hr />
       </div>
-      <div>
-        <div className="boardList">
-          <div className="boardTitleContainer">
-            <p className="boardTitle">게시판 제목</p>
-            <p className="boardContent">내용</p>
-          </div>
-        </div>
+      <div className="boardList">
+        <h3>{title[1]}</h3>
+        <p>내용</p>
+        <hr />
+      </div>
+      <div className="boardList">
+        <h3>{title[2]}</h3>
+        <p>내용</p>
+        <hr />
       </div>
     </div>
   );
