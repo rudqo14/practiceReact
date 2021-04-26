e3dcc7689c22bb3ce3431513d9f07a461d9aa907
김경배 <keyoungbaekim@gimgyeongbaeui-MacBookPro.local>
2021-04-27 06:07:32 +0900

    Add: map을 사용하여 반복되는 코드 수정과 모달창 적용

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.scss";
 
 function App() {
   let [title, setTitle] = useState(["일상 생활", "개발 서적", "알고리즘"]);
-  let [likeBtn, setLikeBtn] = useState(0);
+  let [likeBtn, setLikeBtn] = useState([0, 2, 1]);
   let [modal, setModal] = useState(false);
-
-  const likeHandler = () => {
-    setLikeBtn(likeBtn + 1);
-  };
 
   const firstTitleHandler = () => {
     const newArray = [...title];
@@ -16,14 +12,22 @@ function App() {
       ? (newArray[0] = "나의 이야기")
       : (newArray[0] = "일상 생활");
     setTitle(newArray);
-    /*5Array, Object state 데이터 수정 방법(그냥 카피할 경우 값 공유만)
+    /*Array, Object state 데이터 수정 방법(그냥 카피할 경우 값 공유만)
     -변경함수를 사용함(대체할 데이터)
     -State는 직적 변경하지 않는다.
     -deepCopy를 사용하여 수정*/
   };
 
-  const modalTitleHandler = () => {
-    modal ? setModal(false) : setModal(true);
+  const likeHandler = (likeNumbers) => {
+    const newArray = [...likeBtn];
+    newArray[likeNumbers] += 1;
+    setLikeBtn(newArray);
+  };
+  const modalTitleHandler = (e) => {
+    console.log(e.target.innerText);
+    if (e.target.innerText === "알고리즘") {
+      modal ? setModal(false) : setModal(true);
+    }
   };
 
   return (
@@ -31,48 +35,39 @@ function App() {
       <div className="blackNav">
         <div>개발 Blog</div>
       </div>
-      <div className="boardList">
-        <button
-          onClick={() => {
-            firstTitleHandler();
-          }}
-        >
-          <div className="sidebar" />
-          버튼
-        </button>
-        <h3>
-          {title[0] + "   "}
-          <button
-            className="likeBtn"
-            onClick={() => {
-              likeHandler();
-            }}
-          >
-            👍
-          </button>
-          {"   " + likeBtn}
-        </h3>
-        <p>4월 22일 발행</p>
-        <hr />
-      </div>
-      <div className="boardList">
-        <h3>{title[1] + "   "}</h3>
-        <p>내용</p>
-        <hr />
-      </div>
-      <div className="boardList">
-        <h3
-          className="modalTitle"
-          onClick={() => {
-            modalTitleHandler();
-          }}
-        >
-          {title[2] + "   "}
-        </h3>
-        <p>내용</p>
-        <hr />
-        {modal === true ? <Modal /> : null}
-      </div>
+      <button
+        onClick={() => {
+          firstTitleHandler();
+        }}
+      >
+        <div className="sidebar" />
+        버튼
+      </button>
+      {title.map(function (titleArray, likeNumbers) {
+        return (
+          <div className="boardList">
+            <h3
+              onClick={(e) => {
+                modalTitleHandler(e);
+              }}
+            >
+              {titleArray + "   "}
+            </h3>
+            <button
+              className="likeBtn"
+              onClick={() => {
+                likeHandler(likeNumbers);
+              }}
+            >
+              👍
+            </button>
+            {"   " + likeBtn[likeNumbers]}
+            <p>4월 22일 발행</p>
+            <hr />
+          </div>
+        );
+      })}
+      {modal ? <Modal /> : null}
     </div>
   );
 }
