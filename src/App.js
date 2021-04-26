7c9f66eba666b6fab13053640dfa38da821658a9
김경배 <keyoungbaekim@gimgyeongbaeui-MacBookPro.local>
2021-04-26 15:52:59 +0900

    Add: 게시판 제목을 클릭하여 모달창 적용

--- src/App.js
+++ src/App.js
@@ -2,24 +2,28 @@ import React, { useState } from "react";
 import "./App.scss";
 
 function App() {
-  let [title, setTitle] = useState([
-    "일상 생활",
-    "개발 서적",
-    "알고리즘",
-  ]);
+  let [title, setTitle] = useState(["일상 생활", "개발 서적", "알고리즘"]);
   let [likeBtn, setLikeBtn] = useState(0);
+  let [modal, setModal] = useState(false);
 
   const likeHandler = () => {
     setLikeBtn(likeBtn + 1);
   };
+
   const firstTitleHandler = () => {
     const newArray = [...title];
-    title[0] === "일상 생활" ? (newArray[0] = "나의 이야기") : (newArray[0] = "일상 생활");
-    setTitle (newArray);
+    title[0] === "일상 생활"
+      ? (newArray[0] = "나의 이야기")
+      : (newArray[0] = "일상 생활");
+    setTitle(newArray);
     /*5Array, Object state 데이터 수정 방법(그냥 카피할 경우 값 공유만)
     -변경함수를 사용함(대체할 데이터)
     -State는 직적 변경하지 않는다.
     -deepCopy를 사용하여 수정*/
+  };
+
+  const modalTitleHandler = () => {
+    modal ? setModal(false) : setModal(true);
   };
 
   return (
@@ -33,6 +37,7 @@ function App() {
             firstTitleHandler();
           }}
         >
+          <div className="sidebar" />
           버튼
         </button>
         <h3>
@@ -51,13 +56,33 @@ function App() {
         <hr />
       </div>
       <div className="boardList">
-        <h3>{title[1]}</h3>
+        <h3>{title[1] + "   "}</h3>
         <p>내용</p>
         <hr />
       </div>
       <div className="boardList">
-        <h3>{title[2]}</h3>
+        <h3
+          className="modalTitle"
+          onClick={() => {
+            modalTitleHandler();
+          }}
+        >
+          {title[2] + "   "}
+        </h3>
         <p>내용</p>
+        <hr />
+        {modal === true ? <Modal /> : null}
+      </div>
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div>
+      <div className="modal">
+        <h2>제목</h2>
+        <p>날짜</p>
         <hr />
       </div>
     </div>
